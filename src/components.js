0e533a829ba254b795e8d6a5a9bfacf6ef815787
kieran Sweetman <kikisweetman@gmail.com>
2022-07-28 14:32:43 +0200

    added header main footer + finish

--- src/components.js
+++ src/components.js
@@ -1,5 +1,5 @@
 import "./styles.css";
-import { divGen, li, logo, ul, a } from "./utilities";
+import { divGen, li, logo, ul, a, Hr, span } from "./utilities";
 
 import "flowbite";
 
@@ -125,15 +125,66 @@ const foot = () => {
   styles.forEach((style) => {
     body.classList.add(style);
   });
+  //copy right
+  const hr = Hr([
+    "mx-10",
+    "my-6",
+    "border-gray-6",
+    "sm:mx-auto",
+    "lg:my-8",
+    "w-1/2",
+  ]);
+
+  const spanTag = span(
+    [
+      "block",
+      "text-sm",
+      "text-gray-500",
+      "sm:text-center",
+      "mx-auto",
+      "my-auto",
+    ],
+    " © 2022 "
+  );
+  let persSite = a("https://kieransweetman.com/", "Kieran Sweetman™");
+  persSite.classList.add("hover:underline");
+
+  spanTag.appendChild(persSite);
   // links
+
+  //first parse nodes to get only the list elements + links css (ul)
   let links = ul(3);
+  let linkstyles = [
+    "container",
+    "flex",
+    "justify-end",
+    "items-center",
+    "flex-row",
+    "mx-auto",
+    "px-4",
+  ];
 
+  linkstyles.forEach((style) => {
+    links.classList.add(style);
+  });
+
+  // then adding each a tag to each li element
   let children = links.childNodes;
-
   children.forEach((li) => {
-    let link = a("#");
-    link.textContent = "test";
+    let link = null;
+    if (li.id == 0) {
+      let temp = a("#", "Privacy Policy");
+      link = temp;
+    } else if (li.id == 1) {
+      let temp = a("#", "Licensing");
+      link = temp;
+    } else {
+      let temp = a("#", "Contact");
+      link = temp;
+    }
+
     let css = ["mr-4", "hover:underline", "md:mr-6"];
+
     css.forEach((style) => {
       link.classList.add(style);
     });
@@ -144,7 +195,10 @@ const foot = () => {
   // adding to dom
 
   body.appendChild(logo());
+  body.appendChild(spanTag);
   body.appendChild(links);
+  // body.appendChild(hr);
+
   return body;
 };
 
--- src/utilities.js
+++ src/utilities.js
@@ -18,9 +18,10 @@ const logo = () => {
   return ramenLogo;
 };
 
-const a = (href = "") => {
+const a = (href = "", textcontent = "") => {
   const a = document.createElement("a");
   a.href = href;
+  a.textContent = textcontent;
 
   return a;
 };
@@ -33,9 +34,36 @@ const ul = (count = 0) => {
   const ul = document.createElement("ul");
   for (let i = 0; i < count; i++) {
     const list = li();
+    list.id = i;
     ul.appendChild(list);
   }
 
   return ul;
 };
-export { divGen, logo, ul, li, a };
+
+const Hr = (css) => {
+  const hr = document.createElement("hr");
+  if (css.lenght <= 0) {
+    return hr;
+  }
+
+  css.forEach((style) => {
+    hr.classList.add(style);
+  });
+
+  return hr;
+};
+
+const span = (css, textcontent = "") => {
+  let span = document.createElement("span");
+  if (css.lenght <= 0) {
+    return span;
+  }
+  span.textContent = textcontent;
+  css.forEach((style) => {
+    span.classList.add(style);
+  });
+
+  return span;
+};
+export { divGen, logo, ul, li, a, Hr, span };
