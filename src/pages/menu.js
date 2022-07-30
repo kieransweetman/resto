a24233d3142cac57d42ee1b7941687bebbff5a79
kieran Sweetman <kikisweetman@gmail.com>
2022-07-30 15:17:30 +0200

    finished and found comma list error

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,67 +1,68 @@
-import {divGen, a, li, ul, removeContent, h2, p}  from "../utilities";
-
+import { divGen, a, li, ul, removeContent, h2, p } from "../utilities";
 
 const displayContent = () => {
-    let wrapper = document.querySelector('#content-wrapper');
-    removeContent(wrapper);
+  let wrapper = document.querySelector("#content-wrapper");
+  removeContent(wrapper);
 
-    let body = divGen();
-    let title = divGen();
-    let grid = divGen();
-    let card = divGen();
-    
-    
-    //body settings
-    
-    //title settings
-    let menuTitle = h2("Ohayo Ramen Menu");
-    let menuDesc = p('Our broths are made with time and love to produce the most Umami-esque flavors you will ever taste');
-    
-    title.appendChild(menuTitle);
-    title.appendChild(menuDesc);
+  let body = divGen();
+  let title = divGen();
+  let grid = divGen();
+  let cards = [divGen(), divGen()];
 
-    //grid settings
-    let gridStyles = [
-        'grid',
-        'grid-cols-4',
-        'grid-rows-1',
-        'bg-slate-500',
-        'mx-auto'
-    ]
+  //body settings
 
-    gridStyles.forEach(style=>{
-        grid.classList.add(style);
-    })
+  //title settings
+  let menuTitle = h2("Ohayo Ramen Menu");
+  let menuDesc = p(
+    "Our broths are made with time and love to produce the most Umami-esque flavors you will ever taste"
+  );
 
+  title.appendChild(menuTitle);
+  title.appendChild(menuDesc);
 
-    //card settings
+  //grid settings
+  let gridStyles = [
+    "grid",
+    "grid-cols-4",
+    "grid-rows-1",
+    "bg-slate-500",
+    "mx-auto",
+  ];
 
-    let cardStyles = [
-        "items-center", 
-        
-        "rounded-lg",
-        "shadow",
-        "sm:flex",
-        "dark:bg-gray-800", 
-        "dark:border-gray-700",
-    ]
+  gridStyles.forEach((style) => {
+    grid.classList.add(style);
+  });
 
-        const image = new Image();
-        image.src = "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png";
-        card.appendChild(image);
+  //card settings
 
-        card.classList.add(cardStyles);
+  let cardStyles = [
+    "items-center",
 
-        
+    "rounded-lg",
+    "shadow",
+    "sm:flex",
+    "dark:bg-gray-800",
+    "dark:border-gray-700",
+  ];
 
+  cards.forEach((card) => {
+    const image = new Image();
+    image.src =
+      "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png";
+    card.appendChild(image);
+    cardStyles.forEach((style) => {
+      card.classList.add(style);
+    });
+  });
 
-    // dom manip
-    
+  // dom manip
+
+  cards.forEach((card) => {
     grid.appendChild(card);
-    body.appendChild(title);
-    body.appendChild(grid);
-    wrapper.appendChild(body);
-    
-}
+  });
+  body.appendChild(title);
+  body.appendChild(grid);
+  wrapper.appendChild(body);
+};
 
-export {displayContent};
+export { displayContent };
